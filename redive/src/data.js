1d2b4159903882c8296c635c16b8f713b1388fc6
Toni Lamont <tlam212@gmail.com>
2019-12-22 21:20:32 -0500

    initial components setup

---
+++ redive/src/data.js
@@ -0,0 +1,116 @@
+const images = [
+  {
+    id: 1,
+    title: "Towering New York skyscraper",
+    artist_name: "Lucas Franco",
+    img_url: "https://source.unsplash.com/aRTjFXs6HNc",
+    img_type: "Building"
+  },
+  {
+    id: 2,
+    title: "Monochrome Woman With Umbrella",
+    artist_name: "Jesse Williams",
+    img_url: "https://source.unsplash.com/Bj0LJahdZQo",
+    img_type: "Street"
+  },
+  {
+    id: 3,
+    title: "One Day in New York",
+    artist_name: "Valou_c",
+    img_url: "https://source.unsplash.com/Lg5JH54zalg",
+    img_type: "Subway"
+  },
+  {
+    id: 4,
+    title: "New York Subway",
+    artist_name: "Adi Goldstein",
+    img_url: "https://source.unsplash.com/B2sNfHkjagM",
+    img_type: "Subway"
+  },
+  {
+    id: 5,
+    title: "A View From Dumbo",
+    artist_name: "Dylan Freedom",
+    img_url: "https://source.unsplash.com/iEgP_YHs-jM",
+    img_type: "Bridge"
+  },
+  {
+    id: 6,
+    title: "Black White Train Flatiron",
+    artist_name: "Jesse Williams",
+    img_url: "https://source.unsplash.com/jGwyxVSQ9uM",
+    img_type: "Subway"
+  },
+  {
+    id: 7,
+    title: "Verrazano-Narrows",
+    artist_name: "Manny Ribera",
+    img_url: "https://source.unsplash.com/dV_Blc0bols",
+    img_type: "Bridge"
+  },
+  {
+    id: 8,
+    title: "Urban Wall",
+    artist_name: "Tatiana Rodriguez",
+    img_url: "https://source.unsplash.com/m0wZhtII1FU",
+    img_type: "Street"
+  },
+  {
+    id: 9,
+    title: "Brighton Beach",
+    artist_name: "Paulo Silva",
+    img_url: "https://source.unsplash.com/mHv2CDYhDWU",
+    img_type: "Street"
+  },
+  {
+    id: 10,
+    title: "The Stock Exchange",
+    artist_name: "Aditya Vyas",
+    img_url: "https://source.unsplash.com/mHdATQY9fIU",
+    img_type: "Building"
+  },
+  {
+    id: 11,
+    title: "Standing Tall!",
+    artist_name: "Aditya Vyas",
+    img_url: "https://source.unsplash.com/ZtrahFp1pjA",
+    img_type: "Building"
+  },
+  {
+    id: 12,
+    title: "Person Walks On West Village Street",
+    artist_name: "Chanan Greenblat",
+    img_url: "https://source.unsplash.com/684kZ4oqB6c",
+    img_type: "Street"
+  },
+  {
+    id: 13,
+    title: "Looking Up At The Manhattan Bridge",
+    artist_name: "Hieu Vu Minh",
+    img_url: "https://source.unsplash.com/IRkAbNRT_Yg",
+    img_type: "Bridge"
+  },
+  {
+    id: 14,
+    title: "Statue Of Liberty",
+    artist_name: "James Pond",
+    img_url: "https://source.unsplash.com/hqpG5Ijhsyo",
+    img_type: "Monument"
+  },
+  {
+    id: 15,
+    title: "102 Stops Elevator Ride To Heaven",
+    artist_name: "Mark Ashtoff",
+    img_url: "https://source.unsplash.com/Y_7wXiRn_J8",
+    img_type: "Building"
+  },
+  {
+    id: 16,
+    title: "Lady Liberty",
+    artist_name: "Lucas Franco",
+    img_url: "https://source.unsplash.com/NMlad_sUGTo",
+    img_type: "Monument"
+  },
+
+]
+export default images
